9f196d5873b411e980562fee1d3f9635c888c550
David Spencer <david.spencer@atomicjolt.com>
2018-03-22 17:27:40 -0600

    Finish coarse center detection

--- src/python/test_eye.py
+++ src/python/test_eye.py
@@ -2,65 +2,103 @@ import sys
 import math
 import numpy as np
 import scipy.ndimage.filters as filters
+from skimage.feature import peak_local_max
+
 from pandas import *
+sys.path.append('/usr/local/lib/python3.6/site-packages')
+
 import cv2
 
 # Load an color image in grayscale
-img = cv2.imread('Eye.jpg')
+img = cv2.imread('sam_eye_2.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-iris_radius = 33.0
 
-iris_min = iris_radius - iris_radius * 0.2
-iris_max = iris_radius + iris_radius * 0.2
+iris_radius = 25.0
+
+iris_min = iris_radius - iris_radius * 0.1
+iris_max = iris_radius + iris_radius * 0.1
 iris_min = iris_min * iris_min
 iris_max = iris_max * iris_max
 
-kernel_size = 45
+# Should be a bit larger than 2x the iris radius max, and preferably odd.
+kernel_size = 55
+
 oca = np.zeros((kernel_size, kernel_size), complex)
 wa = np.zeros((kernel_size, kernel_size))
 
-for m in range(1, kernel_size + 1):
-    for n in range(1, kernel_size + 1):
+bounds = int((kernel_size - 1) / 2)
+
+for m in range(-1 * bounds, bounds + 1):
+    for n in range(-1 * bounds, bounds + 1):
         if iris_max > (m*m + n*n):
-            c = 1/math.sqrt(m*m + n*n)
-            wa[m-1][n-1] = c
+            c = 0
+            if m > 0 or n > 0:
+                c = 1/math.sqrt(m*m + n*n)
+
+            wa[m + bounds][n + bounds] = c
+
             if (iris_min < (m*m + n*n)):
-                theta = math.atan(n/m)
-                oca[m-1][n-1] = complex(math.cos(theta) * c, math.sin(theta) * c)
+                theta = 0
+                if n == 0:
+                    theta = math.pi / 2 if m > 0 else -1 * math.pi / 2
+                else:
+                    theta = math.atan(m/n)
+
+                oca[m + bounds][n + bounds] = complex(math.cos(theta) * c, math.sin(theta) * c)
+
+print(oca)
 
 
-beta = 1.5
+beta = 2
 crcc = beta * cv2.Scharr(oca.real, cv2.CV_64F, 1, 0) + (1/beta) * cv2.Scharr(oca.imag, cv2.CV_64F, 0, 1)
 
 
-lam = 0.9
+lam = 0.5
 weighted_img = (1 - lam) * cv2.filter2D(255 - gray, cv2.CV_64F, wa)
 filtered_img = lam * cv2.filter2D(gray, cv2.CV_64F, crcc)
-c = filtered_img + weighted_img
+co = filtered_img + weighted_img
 
-threshold = 300
-data_max = filters.maximum_filter(c, 11)
-data_min = filters.minimum_filter(c, 11)
+# adjust this value down for darker images
+threshold = 500
+
+data_max = filters.maximum_filter(co, 11)
+data_min = filters.minimum_filter(co, 11)
 diff = ((data_max - data_min) > threshold)
-maxima = (c == data_max)
+maxima = (co == data_max)
 maxima[diff == 0] = 0
 max_coords = np.transpose(np.where(maxima))
 
+abs_max = co.max()
+abs_maxima = (co == abs_max)
+abs_coords = np.transpose(np.where(abs_maxima))
+print(abs_max)
+
+max_psr = -1
+max_psr_coords = []
 
 for coords in max_coords:
     row = coords[0]
     col = coords[1]
     print(coords)
     cv2.rectangle(img,(col, row),(col + 2,row + 2),(0,0,255),2)
-    roi_max = c[row-5:row+6, col-5:col+6]
+    roi_max = co[row-5:row+6, col-5:col+6]
     mu = roi_max.mean()
     dev = roi_max.std()
-    print(mu)
-    print(dev)
-    psr = (c[row][col] - mu) / dev
+    #print(mu)
+    #print(dev)
+    psr = (co[row][col] - mu) / dev
     print(psr)
+    if psr > max_psr:
+        max_psr = psr
+        max_psr_coords = coords
+
+row = max_psr_coords[0]
+col = max_psr_coords[1]
+cv2.rectangle(img,(col, row),(col + 2,row + 2),(0,255,0),2)
+#cv2.rectangle(img,(abs_coords[0][1], abs_coords[0][0]), (abs_coords[0][1] + 2, abs_coords[0][0] + 2), (255,255,0), 2)
+print(max_psr)
 
 cv2.imshow('image',img)
 cv2.waitKey(0)

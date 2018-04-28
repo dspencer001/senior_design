e84d2ef0404631fbeaa46701830ed5114e5acb64
Samuel Jungert <itsmesam_123@live.com>
2018-04-28 16:03:28 -0600

    Corner_Detect

---
+++ src/python/test_eye_edit.py
@@ -0,0 +1,133 @@
+import sys
+import math
+import numpy as np
+import scipy.ndimage.filters as filters
+from scipy.signal import medfilt
+
+sys.path.append('/usr/local/lib/python3.6/site-packages')
+
+from track_ic.ellipse_fitting import *
+from track_ic.corner_detection import *
+
+from pandas import *
+
+import cv2
+
+# Load an color image in grayscale
+img = cv2.imread('david_eye_2.jpg')
+#capture = cv2.VideoCapture(2)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+iris_radius = 28.0
+
+rad_min = iris_radius - iris_radius * 0.10
+rad_max = iris_radius + iris_radius * 0.10
+iris_min = rad_min * rad_min
+iris_max = rad_max * rad_max
+
+# Should be a bit larger than 2x the iris radius max, and preferably odd.
+kernel_size = 60
+
+oca = np.zeros((kernel_size, kernel_size), complex)
+wa = np.zeros((kernel_size, kernel_size))
+
+bounds = int((kernel_size - 1) / 2)
+
+for m in range(-1 * bounds, bounds + 1):
+    for n in range(-1 * bounds, bounds + 1):
+        if iris_max > (m*m + n*n):
+            c = 0
+            if m > 0 or n > 0:
+                c = 1/math.sqrt(m*m + n*n)
+
+            wa[m + bounds][n + bounds] = c
+
+            if (iris_min < (m*m + n*n)):
+                theta = 0
+                if n == 0:
+                    theta = math.pi / 2 if m > 0 else -1 * math.pi / 2
+                else:
+                    theta = math.atan(m/n)
+
+                oca[m + bounds][n + bounds] = complex(math.cos(theta) * c, math.sin(theta) * c)
+
+print(oca)
+
+
+beta = 2
+crcc = beta * cv2.Scharr(oca.real, cv2.CV_64F, 1, 0) + (1/beta) * cv2.Scharr(oca.imag, cv2.CV_64F, 0, 1)
+
+
+lam = 0.5
+weighted_img = (1 - lam) * cv2.filter2D(255 - gray, cv2.CV_64F, wa)
+filtered_img = lam * cv2.filter2D(gray, cv2.CV_64F, crcc)
+co = filtered_img + weighted_img
+
+# adjust this value down for darker images
+threshold = 1800
+
+data_max = filters.maximum_filter(co, 11)
+data_min = filters.minimum_filter(co, 11)
+diff = ((data_max - data_min) > threshold)
+maxima = (co == data_max)
+maxima[diff == 0] = 0
+max_coords = np.transpose(np.where(maxima))
+
+abs_max = co.max()
+abs_maxima = (co == abs_max)
+abs_coords = np.transpose(np.where(abs_maxima))
+print(abs_max)
+
+max_psr = -1
+max_psr_coords = []
+
+for coords in max_coords:
+    row = coords[0]
+    col = coords[1]
+    print(coords)
+    cv2.rectangle(img,(col, row),(col + 2,row + 2),(0,0,255),2)
+    roi_max = co[row-5:row+6, col-5:col+6]
+    mu = roi_max.mean()
+    dev = roi_max.std()
+    #print(mu)
+    #print(dev)
+    psr = (co[row][col] - mu) / dev
+    if psr > max_psr:
+        max_psr = psr
+        max_psr_coords = coords
+
+row = max_psr_coords[0]
+col = max_psr_coords[1]
+
+print(row)
+print(col)
+
+c_image = corner_detect(gray)
+
+#center, width, height, phi = fit_ellipse((row, col), gray, rad_max, rad_min)
+##candidate_points = fit_ellipse((row, col), gray, rad_max, rad_min)
+##for pt in candidate_points:
+##    cv2.rectangle(img, (pt[1],pt[0]), (pt[1] + 1, pt[0] + 1), (0,255,0), 1)
+#
+#
+##cv2.ellipse(img, (center[1], center[0]), (width / 2, height / 2), phi, (0, 360), 255, 2)
+#pixel_center = np.round(center).astype(int)
+#width = round(width).astype(int)
+#height = round(height).astype(int)
+#
+#cv2.ellipse(img,(pixel_center[0], pixel_center[1]),(width, height),phi,0,360,(0,255,0),1)
+#
+#
+#cv2.rectangle(img,(col, row),(col + 2,row + 2),(0,255,0),2)
+#cv2.rectangle(img,(pixel_center[0], pixel_center[1]),(pixel_center[0] + 2,pixel_center[1] + 2),(0,0,255),2)
+#
+##cv2.rectangle(img,(abs_coords[0][1], abs_coords[0][0]), (abs_coords[0][1] + 2, abs_coords[0][0] + 2), (255,255,0), 2)
+#print(max_psr)
+
+#cv2.imshow('image',img)
+cv2.imshow('image', c_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
--- src/python/track_ic/corner_detection.py
+++ src/python/track_ic/corner_detection.py
@@ -1,38 +1,77 @@
 import cv2
 import numpy as np
+from scipy import ndimage as ndi
+import skimage
+from skimage.util import img_as_float
+from skimage.filters import gabor_kernel
 
-#filename = 'chessboard2.jpg'
-#img = cv2.imread(filename)
-#gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+def corner_detect(input_image):
+    def build_filters():
+        filters = []
+        ksize = 3
+        for theta in np.arange(0, np.pi, np.pi / 16):
+            for sigma in (1, 3):
+                for freq in (0.05, 0.25):
+                    kern = cv2.getGaborKernel((ksize, ksize), 5.0, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
+                    kern /= 1.5*kern.sum()  
+                    filters.append(kern)
+        return filters
+    
+    kernels = []
+    for theta in range(4):
+        theta = theta / 4. * np.pi
+        for sigma in (1, 3):
+            for frequency in (0.05, 0.25):
+                kernel = np.real(gabor_kernel(frequency, theta=theta,
+                                              sigma_x=sigma, sigma_y=sigma))
+                kernels.append(kernel)
 
-def corner_detect(gray_input, img_input, row, col):
-    height, width =gray_input.shape
-    print(height, width)
-    gray = gray_input
-    img = img_inputimg = cv2.imread("lenna.png")
-crop_img = img[y:y+h, x:x+w]
-    #gray = gray_input[col+col:h, row+row:w]
-    #img = img_input[col+col:h, row+row:w]
-    # find Harris corners
-    gray = np.float32(gray)
-    dst = cv2.cornerHarris(gray,2,3,0.004) #0.04
-    dst = cv2.dilate(dst,None)
-    ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0) #0.01*dst.max() was original value
-    dst = np.uint8(dst)    
 
-    # find centroids
-    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)  
+    def process(img, filters):
+        accum = np.zeros_like(img)
+        for kern in filters:
+            fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
+            np.maximum(accum, fimg, accum)
+        return accum
+    
+    def compute_feats(image, kernels):
+        feats = np.zeros((len(kernels), 2), dtype=np.double)
+        for k, kernel in enumerate(kernels):
+            filtered = ndi.convolve(image, kernel, mode='wrap')
+            feats[k, 0] = filtered.mean()
+            feats[k, 1] = filtered.var()
+        return feats
+    
+    def match(feats, ref_feats):
+        min_error = np.inf
+        min_i = None
+        for i in range(ref_feats.shape[0]):
+            error = np.sum((feats - ref_feats[i, :])**2)
+            if error < min_error:
+                min_error = error
+                min_i = i
+        return min_i
 
-    # define the criteria to stop and refine the corners
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)  
+    def power(image, kernel):
+    # Normalize images for better comparison.
+        image = (image - image.mean()) / image.std()
+        return np.sqrt(ndi.convolve(image, np.real(kernel), mode='wrap')**2 +
+                   ndi.convolve(image, np.imag(kernel), mode='wrap')**2)
+    
+    img = input_image
 
-    # Now draw them
-    res = np.hstack((centroids,corners))
-    res = np.int0(res)
-    img[res[:,1],res[:,0]]=[0,0,255]
-    img[res[:,3],res[:,2]] = [0,255,0]
+    if img is None:
+        print('Failed to load image file!')
+        sys.exit(1)
 
-    cv2.imwrite('subpixel5.png',img)
-
-    return img
+    filters = build_filters()
+    power(img, filters)
+    res1 = process(img, filters)
+    # prepare reference features
+    ref_feats = np.zeros((3, len(filters), 2), dtype=np.double)
+    ref_feats[0, :, :] = compute_feats(input_image, filters)
+    match(res1, ref_feats)
+    cv2.imshow('result', res1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() 
+    return
